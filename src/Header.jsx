6fc3b051b324254c5d0bd9674214422609cb44b0
Deepanshu7348 <108023848+Deepanshu7348@users.noreply.github.com>
2023-10-18 22:32:04 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
+
+function Header({OpenSidebar}) {
+  return (
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
+        </div>
+        <div className='header-left'>
+            {/* <BsSearch  className='icon'/> */}
+            <p>Hello Deepanshu</p>
+        </div>
+        <div className='header-right'>
+            {/* <BsFillBellFill className='icon'/>
+            <BsFillEnvelopeFill className='icon'/>
+            <BsPersonCircle className='icon'/> */}
+            <BsSearch  className='icon'/>
+        </div>
+    </header>
+  )
+}
+
+export default Header
